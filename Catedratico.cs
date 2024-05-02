10a60fc5663b1389ee74d812d9c32128bf1c484a
gramz <gramz@LAB4-CAT4>
2024-05-02 09:23:10 -0600

    Se agrego un segundo formulario

--- Catedratico.cs
+++ Catedratico.cs
@@ -4,11 +4,11 @@ namespace BlazorAppFormulariosValidacion
 {
     public class Catedratico : Persona
     {
-        [Required(ErrorMessage = "El Título es Requerido")]
+        [Required (ErrorMessage = "El Título es Requerido")]
         public string Titulo { get; set; }
-        [Required(ErrorMessage = "El Colegiado")]
-        [StringLength(6,
-        ErrorMessage = "Deben ser menos de 6 dígitos")]
+
+        [Required(ErrorMessage = "El Colegiado es Requerido")]
+        [StringLength(6,ErrorMessage = "Deben ser menos de 6 dígitos")]
         public string Colegiado { get; set; }
 
         public Catedratico()
